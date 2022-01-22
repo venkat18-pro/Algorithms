1c544a1e9c3e5e0eb9461da8a8e3b8b731bc56cb
Venkatesh <viv7012@gmail.com>
2022-01-22 21:12:27 +0530

    Binary Search Algorithm with java.

---
+++ src/com/algorithms/java/BinarySearch.java
@@ -0,0 +1,37 @@
+package com.algorithms.java;
+
+public class BinarySearch {
+
+	public int  binarySearch(int []arr, int start, int length, int x) {
+		
+		if(length >= 3) {
+			int mid = start +(length - 1) / 2;
+			
+			if(arr[mid] == x)
+				return mid;
+			
+			if(arr[mid] > x)
+				return binarySearch(arr, start, mid - 1, x);
+			
+			return binarySearch(arr, mid + 1, length, x);
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		BinarySearch bin = new BinarySearch();
+		int [] arr = {1, 3, 4, 10, 40};
+		int len = arr.length;
+		int x = 10;
+		
+		int result = bin.binarySearch(arr, 0, len - 1, x);
+		
+		if(result == -1)
+			System.out.println("Element is not presend");
+		else
+			System.out.println("Element fount at Indix: "+result);
+	}
+
+}
